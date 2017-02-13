37f0375ade3a9ee6572eb234a8f3f2174f1782a6
Alexei Skazik <askazik@mail.ru>
2017-02-13 13:40:54 +0300

    Calculate B. Remember about 07/12/2016 antenn system change.  Append script for view of ionogram files in the <ion> directory.

--- frq/get_amplitudes.sql
+++ frq/get_amplitudes.sql
@@ -21,3 +21,23 @@ amplitudes.ampl_file = files.id_file
 AND (TIME(files.time) >= '18:00:00' OR TIME(files.time) < '07:00:00')
 GROUP BY amplitudes.ampl_file  HAVING COUNT(*)>1
 )
+
+-- Номера файлов и частот для которых найдены два первых отражения.
+SELECT DISTINCT 
+amplitudes.ampl_file AS i_file,
+amplitudes.ampl_frq AS i_frq
+FROM 
+amplitudes
+WHERE 
+amplitudes.number == 1
+
+-- Выборки с посчитанными B
+SELECT 
+id_ampl, filename, time, frequency, B_with_noise, B_without_noise
+FROM 
+files, frequencies, amplitudes
+WHERE 
+files.id_file == amplitudes.ampl_file
+AND frequencies.id_frq = amplitudes.ampl_frq
+AND B_with_noise  <> 0
+-- AND (TIME(time) >= '18:00:00' OR TIME(time) < '07:00:00')
