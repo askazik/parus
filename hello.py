52d3c4db896980f3d35c79d1b5f639cceadac3c3
Alexey Skazik <askazik@zuzex.com>
2018-03-26 18:02:11 +0300

    test mat->python

--- hello.py
+++ hello.py
@@ -1,30 +1,90 @@
 # -*- coding: utf-8 -*-
+import sys
+import os.path
 
 import scipy.io
 import numpy as np
 import h5py
-import os.path
-
-print("Hello world!")
-
-# mat version < 7.3
-# save('test.mat','-v7')
-mat = scipy.io.loadmat('file.mat')
-
-# mat version >= 7.3
-# h5py extension requires HDF5 on your system.
-# This works fine, if you use the '-v7.3' flag in Matlab when saving out your data.
-f = h5py.File('somefile.mat', 'r')
-data = f.get('data/variable1')
-data = np.array(data)  # For converting to numpy array
 
 
 class ClassMatFile:
     """Класс осуществляет обработку v7 и hdf5 mat-файлов Matlab."""
 
-    def __init__(self, filename):
+    extensions = ['mat', 'fig'] # расширения файла
 
-        if not os.path.isfile(filename):
-            message = "No such file or directory <{}>!".format(filename)
+    @property
+    def filename(self): return self._filename
+    @filename.setter
+    def filename(self, value):
+
+        # Проверка на существование файла.
+        if not os.path.isfile(value):
+            message = "No such file or directory <{}>!".format(value)
             raise OSError(message)
-        self.filename = filename
+
+        # Проверка на соответствие расширения
+        extension = value.split('.')[-1].lower()
+        if value.lower() == extension:
+            message = "The file <{}> have not extension!".format(value)
+            raise OSError(message)
+        else:
+            if extension not in self.extensions:
+                message = "The file <{}> isn't mat-file!".format(value)
+                raise OSError(message)
+
+        # Пытаемся открыть mat файл
+        try:
+            self._open_old_mat(value)
+        except Exception as e:
+            print(e)
+            self._open_hdf5_mat(value)
+        else:
+            self._filename = value
+
+    def _open_old_mat(self, filename):
+        """Пытаемся открыть файл старого формата."""
+        # mat version < 7.3, save('test.mat','-v7')
+        try:
+            mat = scipy.io.loadmat(filename)
+        except:
+            message = "The file <{}> isn't mat-file with version < 7.3!".format(filename)
+            raise OSError(message)
+        else:
+            self._source = mat
+            self._version = 'old'
+
+
+    def _open_hdf5_mat(self, filename):
+        """Пытаемся открыть файл формата hdf5."""
+        # mat version >= 7.3
+        # h5py extension requires HDF5 on your system.
+        # This works fine, if you use the '-v7.3' flag in Matlab when saving out your data.
+        try:
+            File = h5py.File(filename, 'r')
+        except:
+            message = "The file <{}> isn't mat-file with version >= 7.3!".format(filename)
+            raise OSError(message)
+        else:
+            self._source = File
+            self._version = 'hdf5'
+
+
+    def __init__(self, filename):
+        """Инициализация объекта файлом mat произвольного формата."""
+        self._source = False
+        self._version = False
+
+        try:
+            self.filename = filename
+        except OSError as e:
+            print(e)
+            raise
+
+
+if __name__ == "__main__":
+
+    print("Hello world!")
+    A = ClassMatFile('db_interface.fig')
+
+    #data = f.get('data/variable1')
+    #data = np.array(data)  # For converting to numpy array
--- mat_tests.py
+++ mat_tests.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 def fun(x):
     return x + 1
 
-class MyTest(unittest.TestCase):
+class MatTest(unittest.TestCase):
     def test(self):
         self.assertEqual(fun(3), 4)
